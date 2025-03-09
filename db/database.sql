b459279ea3fe637b9b5e22f93ba8450381f4df69
SantiCrudo <santijcrudo@gmail.com>
2025-03-09 18:52:53 -0300

    start script added

---
+++ db/database.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS companydb;
+
+USE companydb;
+
+CREATE TABLE employee (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) DEFAULT NULL,
+    salary INT(5) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
+
+DESCRIBE employee;
+
+INSERT INTO employee VALUES
+    (1, 'Joe', 1000),
+    (2, 'Gae', 2000),
+    (3, 'Eoea', 5000),
+    (4, 'Feae', 4000);
---
+++ src/db.js
@@ -0,0 +1,18 @@
+import {createPool} from 'mysql2/promise'
+import {
+    DB_HOST,
+    DB_PORT,
+    DB_DATABASE,
+    DB_USER,
+    DB_PASSWORD
+} from './config.js'
+
+// crear conexión
+
+export const pool = createPool({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PASSWORD,
+    database: DB_DATABASE
+})
